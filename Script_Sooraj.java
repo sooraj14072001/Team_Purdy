42945b0847cef4b80b9119af03f0c6f5218732fd
sooraj14072001 <88287926+sooraj14072001@users.noreply.github.com>
2021-10-15 00:07:31 +0530

    Update Script_Sooraj.java

--- Script_Sooraj.java
+++ Script_Sooraj.java
@@ -2,10 +2,10 @@ public class script_sooraj
  {
   public static void main (String[] args)
   {
-   String name = "Sooraj S";
+   String name = "Sooraj Shivakumar";
    String email = "sooraj14072001@gmail.com";
    String slack= "@Sooraj";
-   String biostack = "Genomics";
-   System.out.println(name + ", " + email + ", " + slack + ", " + biostack + ", " );
+   String biostack = "Genomics / Microbiology";
+   System.out.println(name + ", " + email + ", " + slack + ", " + biostack );
    }
  }
