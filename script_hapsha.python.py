59376e1870124244023dde8b151ce26cfe2a85dc
hapsha-ava <92299571+hapsha-ava@users.noreply.github.com>
2021-10-15 14:13:39 +0600

    Delete script_hapsha.python.py

--- script_hapsha.python.py
+++
@@ -1,6 +0,0 @@
-name = "Tanvironnessa Hapsha"
-email = "tanvironnessa20@student.sust.edu"
-slack_username = "@Hapsha"
-biostack = "Genomics"
-
-print ("The name of the user is: "+name+"\nThe email address of the user is: "+email+"\nThe slack username of the user is: "+slack_username+"\nThe Biostack of the user is: "+biostack) 
