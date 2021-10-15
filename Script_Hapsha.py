9339dd1b17dbdb7604a00615fd0ae4908961d530
hapsha-ava <92299571+hapsha-ava@users.noreply.github.com>
2021-10-15 16:01:26 +0600

    Add files via upload

---
+++ Script_Hapsha.py
@@ -0,0 +1,6 @@
+name = "Tanvironnessa Hapsha"
+email = "tanvironnessa20@student.sust.edu"
+slack_username = "@Hapsha"
+biostack = "Genomics"
+
+print (name+", "+email+", "+slack_username+", "+biostack) 
