9ad22cac83cbef4b4b23300b11edef8c11cbb304
hapsha-ava <92299571+hapsha-ava@users.noreply.github.com>
2021-10-16 11:46:13 +0600

    Delete Script_Hapsha.py

--- Script_Hapsha.py
+++
@@ -1,6 +0,0 @@
-name = "Tanvironnessa Hapsha"
-email = "tanvironnessa20@student.sust.edu"
-slack_username = "@Hapsha"
-biostack = "Genomics"
-
-print (name+", "+email+", "+slack_username+", "+biostack) 
